d5d348e3ec781c3247d167406243295c1868524d
henriquepablo <pablo269704@gmail.com>
2025-09-17 19:09:35 -0300

    feat: create component card payment view

--- PayUp/Sources/Core/Components/PaymentCardView/PaymentCardView.swift
+++ PayUp/Sources/Core/Components/PaymentCardView/PaymentCardView.swift
@@ -19,6 +19,40 @@ final class PaymentCardView: UIView {
         return view
     }()
     
+    let iconImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "callendarDollar"))
+        image.contentMode = .scaleAspectFit
+        image.tintColor = Colors.accentOrange
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "A receber"
+        label.font = Fonts.paragraphSmall()
+        label.textColor = Colors.textParagraph
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Aurora Tech Soluções Digitais"
+        label.font = Fonts.titleSmall()
+        label.textColor = Colors.textHeading
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.text = "R$ 250,00"
+        label.font = Fonts.paragraphMedium()
+        label.textColor = Colors.textParagraph
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,7 +65,10 @@ final class PaymentCardView: UIView {
     
     private func setup() {
         addSubview(container)
-        
+        container.addSubview(iconImage)
+        container.addSubview(subtitleLabel)
+        container.addSubview(nameLabel)
+        container.addSubview(valueLabel)
         setupConstraints()
     }
     
@@ -40,7 +77,28 @@ final class PaymentCardView: UIView {
             container.topAnchor.constraint(equalTo: topAnchor),
             container.leadingAnchor.constraint(equalTo: leadingAnchor),
             container.trailingAnchor.constraint(equalTo: trailingAnchor),
-            container.bottomAnchor.constraint(equalTo: bottomAnchor)
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            iconImage.topAnchor.constraint(equalTo: container.topAnchor, constant: 16),
+            iconImage.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+            iconImage.widthAnchor.constraint(equalToConstant: 24),
+            iconImage.heightAnchor.constraint(equalToConstant: 24),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: iconImage.topAnchor),
+            subtitleLabel.leadingAnchor.constraint(equalTo: iconImage.trailingAnchor, constant: 16),
+            
+            nameLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 4),
+            nameLabel.leadingAnchor.constraint(equalTo: subtitleLabel.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -32),
+            
+            valueLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4),
+            valueLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            valueLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -32)
         ])
+    }
+    
+    func configure(name: String, value: String) {
+        nameLabel.text = name
+        valueLabel.text = value
     }
 }
